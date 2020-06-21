90dc8dadd339f8c710ca2a40ddaeba6e34553f2c
mvindigni <42945905+mvindigni@users.noreply.github.com>
2020-06-21 14:38:44 -0400

    parses table for champ names and puts them in list

--- scrape.py
+++ scrape.py
@@ -1,24 +1,29 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions as ec
 
 driver = webdriver.Chrome("chromedriver.exe")
 driver.get("https://leagueoflegends.fandom.com/wiki/List_of_champions")
 
 try:
     element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.XPATH, '//*[@id="mw-content-text"]/table[2]'))
+        ec.presence_of_element_located((By.XPATH, '//*[@id="mw-content-text"]/table[2]/tbody'))
     )
 finally:
-    champList = driver.find_elements_by_xpath('//*[@id="mw-content-text"]/table[2]')
+    champTable = driver.find_element_by_xpath('//*[@id="mw-content-text"]/table[2]/tbody')
+
+champList = champTable.find_elements(By.TAG_NAME, "tr")
 
 champions = []
+counter = 1
 
-for champ in champList:
-    champions.append(champ.find_element_by_xpath(
-        '// *[ @ id = "mw-content-text"] / table[2] / tbody / tr[1] / td[1]'
-    ).get_attribute("data-sort-value"))
+for row in champList:
+    name = row.find_element_by_xpath(
+        '// *[ @ id = "mw-content-text"] / table[2] / tbody / tr[' + str(counter) + '] / td[1]'
+    )
+    champions.append(name.get_attribute('data-sort-value'))
+    counter += 1
 
 driver.close()
 
